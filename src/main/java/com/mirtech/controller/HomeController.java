24de652631005cc9a2f6fb255c6409a7c7e536da
gayoung <ig1854@nate.com>
2026-03-20 14:16:59 +0900

    견적문의

--- src/main/java/com/mirtech/controller/HomeController.java
+++ src/main/java/com/mirtech/controller/HomeController.java
@@ -1,12 +1,22 @@
 package com.mirtech.controller;
 
 import com.mirtech.entity.Inquiry;
+import com.mirtech.service.InquiryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class HomeController {
+
+    private final InquiryService inquiryService;
+
+    public HomeController(InquiryService inquiryService) {
+        this.inquiryService = inquiryService;
+    }
 
     @GetMapping("/")
     public String home(Model model) {
@@ -14,4 +24,14 @@ public class HomeController {
         return "home/index";
     }
 
+    @PostMapping("/inquiry")
+    public String submitFromHome(@ModelAttribute Inquiry inquiry, RedirectAttributes redirectAttributes){
+        try {
+            inquiryService.submit(inquiry);
+            redirectAttributes.addFlashAttribute("successMsg", "문의가 정상적으로 접수되었습니다.");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMsg", "오류가 발생했습니다. 다시 시도해 주세요.");
+        }
+        return "redirect:/#inquiry";
+    }
 }
