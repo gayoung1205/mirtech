b4a2e179f1b228292673422be6150c0d6f7a7b14
gayoung <ig1854@nate.com>
2026-04-16 15:22:46 +0900

    배너 이미지 수정중

--- src/main/java/com/mirtech/config/WebMvcConfig.java
+++ src/main/java/com/mirtech/config/WebMvcConfig.java
@@ -1,8 +1,10 @@
 package com.mirtech.config;
 
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,5 +20,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
         String absolutePath = Paths.get(uploadDir).toAbsolutePath().toString();
         registry.addResourceHandler("/uploads/**")
             .addResourceLocations("file:" + absolutePath + "/");
+
+        registry.addResourceHandler("/images/**")
+            .addResourceLocations("classpath:/static/images/")
+            .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+
     }
 }
